e5d5d7ab6028331e8cf564adb59732a8a49ba903
Yvonnick Esnault <yvonnick.esnault@ovh.net>
2015-10-30 17:05:15 +0100

    Initial Revision
    Signed-off-by: Yvonnick Esnault <yvonnick.esnault@ovh.net>

---
+++ README.md
@@ -0,0 +1,17 @@
+# Usage
+
+This is a plugin for Tat Webui, see https://github.com/tat/tatwebui
+
+See Tat Engine for more information: https://github.com/tat/tatcli
+
+## Configuration
+In plugin.tpl.json file, add this line :
+
+```
+"tatwebui-plugin-standardview": "git+https://github.com/ovh/tatwebui-plugin-standardview.git"
+```
+
+# Hacking
+
+You've developed a new cool feature? Fixed an annoying bug? We'd be happy
+to hear from you! Make sure to read [CONTRIBUTING.md](./CONTRIBUTING.md) before.
---
+++ standardview/list/standardview-list.route.js
@@ -0,0 +1,17 @@
+/*global angular*/
+angular.module('TatUi').config(function ($stateProvider, PluginProvider) {
+    'use strict';
+
+    PluginProvider.addPlugin({"name":"Standard View", "route" : "standardview-list", "type": "messages-views"});
+
+    $stateProvider.state('standardview-list', {
+        url: '/standardview/list/{topic:topicRoute}?idMessage&filterInLabel&filterAndLabel&filterNotLabel&filterInTag&filterAndTag&filterNotTag',
+        templateUrl: '../build/tatwebui-plugin-standardview/standardview/list/standardview-list.view.html',
+        controller: 'MessagesStandardViewListCtrl',
+        controllerAs: 'ctrl',
+        reloadOnSearch: false,
+        translations: [
+            'plugins/tatwebui-plugin-standardview/standardview'
+        ]
+    });
+});
