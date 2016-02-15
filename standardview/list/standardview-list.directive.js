7e2a76158b613d1a60afdda5f01f9cab2d8fe8cc
Yvonnick Esnault <yvonnick.esnault@ovh.net>
2016-02-15 23:42:02 +0100

    refactor: message-label as component
    Signed-off-by: Yvonnick Esnault <yvonnick.esnault@ovh.net>

--- standardview/list/standardview-list.directive.js
+++ standardview/list/standardview-list.directive.js
@@ -46,8 +46,7 @@ angular.module('TatUi').directive('messagesStandardviewItem', function($compile)
      * @description Directive controller
      */
     controller: function($scope, $rootScope, TatEngineMessageRsc,
-      TatEngineMessagesRsc, TatEngine,
-      Authentication, $http, appConfiguration) {
+      TatEngineMessagesRsc, TatEngine, TatMessage, Authentication) {
       var self = this;
       this.answerPanel = false;
       this.isTopicBookmarks = false;
@@ -68,35 +67,6 @@ angular.module('TatUi').directive('messagesStandardviewItem', function($compile)
           0.7152 * parseInt(result[2], 16) +
           0.0722 * parseInt(result[3], 16) : 0;
       };
-
-      this.palette = [
-        ["#d04437", "#14892c", "#5484ed"],
-        ["#000", "#444", "#666", "#999", "#ccc", "#eee", "#f3f3f3",
-          "#fff"
-        ],
-        ["#000", "#444", "#666", "#999", "#ccc", "#eee", "#f3f3f3",
-          "#fff"
-        ],
-        ["#f00", "#f90", "#ff0", "#0f0", "#0ff", "#00f", "#90f", "#f0f"],
-        ["#f4cccc", "#fce5cd", "#fff2cc", "#d9ead3", "#d0e0e3",
-          "#cfe2f3", "#d9d2e9", "#ead1dc"
-        ],
-        ["#ea9999", "#f9cb9c", "#ffe599", "#b6d7a8", "#a2c4c9",
-          "#9fc5e8", "#b4a7d6", "#d5a6bd"
-        ],
-        ["#e06666", "#f6b26b", "#ffd966", "#93c47d", "#76a5af",
-          "#6fa8dc", "#8e7cc3", "#c27ba0"
-        ],
-        ["#c00", "#e69138", "#f1c232", "#6aa84f", "#45818e", "#3d85c6",
-          "#674ea7", "#a64d79"
-        ],
-        ["#900", "#b45f06", "#bf9000", "#38761d", "#134f5c", "#0b5394",
-          "#351c75", "#741b47"
-        ],
-        ["#600", "#783f04", "#7f6000", "#274e13", "#0c343d", "#073763",
-          "#20124d", "#4c1130"
-        ]
-      ];
 
       /**
        * @ngdoc function
@@ -127,16 +97,15 @@ angular.module('TatUi').directive('messagesStandardviewItem', function($compile)
       };
 
       this.addLabelDoing = function(message) {
-        message.currentLabel = {
-          text: "doing",
-          color: "#5484ed"
-        };
+        TatMessage.addLabel(message, $scope.topic, "doing", "#5484ed",
+          function() {
+              TatMessage.addLabel(message,
+              $scope.topic,
+              "doing:" + Authentication.getIdentity().username,
+              "#5484ed" //blue
+          )}
+        );
         self.removeLabel(message, "done");
-        self.addLabel(message, function() {
-          message.currentLabel.text = "doing:" + Authentication.getIdentity().username;
-          message.currentLabel.color = "#5484ed"; //blue
-          self.addLabel(message);
-        });
       };
 
       /**
@@ -191,12 +160,8 @@ angular.module('TatUi').directive('messagesStandardviewItem', function($compile)
       };
 
       this.addLabelDone = function(message) {
-        message.currentLabel = {
-          text: "done",
-          color: "#14892c" // green
-        };
+        TatMessage.addLabel(message, $scope.topic, "done", "#14892c"); // green
         self.removeLabel(message, "doing");
-        self.addLabel(message);
         self.canDoneMessage = false;
       };
 
@@ -241,8 +206,7 @@ angular.module('TatUi').directive('messagesStandardviewItem', function($compile)
        * @description create a bookmark from on message
        */
       this.bookmarkMessage = function() {
-        var to = 'Private/' + Authentication.getIdentity().username +
-          '/Bookmarks';
+        var to = 'Private/' + Authentication.getIdentity().username + '/Bookmarks';
         TatEngineMessageRsc.create({
           'idReference': $scope.message._id,
           'action': 'bookmark',
@@ -342,37 +306,6 @@ angular.module('TatUi').directive('messagesStandardviewItem', function($compile)
 
       /**
        * @ngdoc function
-       * @name addLabel
-       * @methodOf TatUi.controller:messagesItem
-       * @description Add a label
-       * @param {object} message Message on which to add a label
-       */
-      this.addLabel = function(message, cb) {
-        TatEngineMessageRsc.update({
-          'action': 'label',
-          'topic': $scope.topic,
-          'idReference': $scope.message._id,
-          'text': message.currentLabel.text,
-          'option': message.currentLabel.color
-        }).$promise.then(function(resp) {
-          if (!message.labels) {
-            message.labels = [];
-          }
-          message.labels.push({
-            text: message.currentLabel.text,
-            color: message.currentLabel.color
-          });
-          message.currentLabel.text = '';
-          if (cb) {
-            cb();
-          }
-        }, function(resp) {
-          TatEngine.displayReturn(resp);
-        });
-      };
-
-      /**
-       * @ngdoc function
        * @name removeLabel
        * @methodOf TatUi.controller:messagesItem
        * @description remove a label
@@ -419,24 +352,6 @@ angular.module('TatUi').directive('messagesStandardviewItem', function($compile)
         });
       };
 
-      /**
-       * @ngdoc function
-       * @name initColor
-       * @methodOf TatUi.controller:messagesItem
-       * @description Initialize the color of the picker
-       * @param {object} message Message on which the color picker will be initialized
-       * @return {true} Allways return true as it is used in an ng-show
-       */
-      this.initColor = function(message) {
-        if (!message.currentLabel) {
-          message.currentLabel = {};
-        }
-        if (!message.currentLabel.color) {
-          message.currentLabel.color = '#d04437';
-        }
-        return true;
-      };
-
       this.containsLabel = function(message, labelText) {
         if (message.inReplyOfIDRoot) {
           return false;
@@ -454,7 +369,6 @@ angular.module('TatUi').directive('messagesStandardviewItem', function($compile)
       };
 
       this.init = function(message) {
-        this.initColor(message);
         if ($scope.topic.indexOf("Private/" + Authentication.getIdentity()
             .username + "/Bookmarks") === 0) {
           self.isTopicBookmarks = true;
